cdf58cd3f649f42c2f1febf338b678bc8465b2a7
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2023-06-27 23:29:30 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -9,8 +9,8 @@ from importlib import import_module
 from sys import argv
 
 # from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-from userbot import LOGS, bot
-from userbot.modules import ALL_MODULES
+# from userbot import LOGS, bot
+# from userbot.modules import ALL_MODULES
 
 
 INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
