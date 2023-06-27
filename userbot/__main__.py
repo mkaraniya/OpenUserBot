03859a145bb1007a9cb0d55bea360c0cb466e5b0
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2023-06-27 23:55:39 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -35,8 +35,9 @@ LOGS.info(
     "Once died 😵, but now ON again 😉, A BIG thanks to https://github.com/Devp73 Dev Patel who have helped make this userbot ON AGAIN with his HUGE FIX!!"
     "for command list type .help & If you need assistance, head to https://t.me/PPE_Support")
 """
-
+"""
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.run_until_disconnected()
+"""
